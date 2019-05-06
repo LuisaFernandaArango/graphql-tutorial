4b1c45ead6f3e5af4c11d382f7169398b961367e
LuisaFernandaArango <luisa_fdarangova@hotmail.com>
2019-05-06 14:56:31 -0500

    Se agrega comentario

--- client/src/components/ChannelsListWithData.js
+++ client/src/components/ChannelsListWithData.js
@@ -34,5 +34,5 @@ export const channelsListQuery = gql`
 `;
 
 export default graphql(channelsListQuery, {
-  options: { pollInterval: 5000 },
+  options: { pollInterval: 5000 }, //Detecta el cambio cada 5 segundos
 })(ChannelsList);
